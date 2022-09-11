ac38e0eae178491fe2f8959c42baed0f2d364c07
Nightlymist <nathalieaouellette@gmail.com>
2022-09-10 20:12:17 -0400

    Fixed everything I think

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -6,10 +6,10 @@ import csv
 import os
 from sqlite3 import Row
 
-# Add a variable to load a file from a path.
-file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_results.txt")
+# Add a variable to load a file from a path.
+file_to_load = os.path.join("resources", "election_results.csv")
 # Initialize a total vote counter.
 total_votes = 0
 # Candidate Options and candidate votes.
